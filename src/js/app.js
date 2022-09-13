e9a14b2632379726fcbc8cfef759083f0b87b143
Ilya <ilyakorckishko@gmail.com>
2022-09-13 17:17:01 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import * as flsFunctions from './modules/functions.js'
+
+flsFunctions.isWebp()
+flsFunctions.menuInit();
+flsFunctions.spollers();
+
+import './modules/popup.js'
+import './modules/script.js'
